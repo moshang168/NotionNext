c714a4c2c7e71f64ed44acc4fe4dd2c72f342206
墨若浮尘 <107167867+moshang168@users.noreply.github.com>
2025-03-25 19:53:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.moshang.eu.org' },
+    { title: '访问主站', url: 'https://www.moshang168.eu.org' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,10 +20,11 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: 'MOSHANG.EU.ORG',
+  HEO_HERO_TITLE_4: '
+welcome',
+  HEO_HERO_TITLE_5: '酒逢知己千杯少，话不投机半句多。',
+  HEO_HERO_TITLE_LINK: 'https://www.moshang168.eu.org',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
